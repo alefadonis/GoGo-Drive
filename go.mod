3d9ce3da079550666bfafc69f60387240f49a993
kmmelcher <kilian.melcher@gmail.com>
2023-06-04 17:42:18 -0300

    feat: Create /upload endpoint

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.20
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
--- src/main.go
+++ src/main.go
@@ -2,39 +2,65 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"encoding/json"
+	"os"
+	"path/filepath"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-type File struct {
-	Name string `json: "Name"`
-}
+const UploadDir = "./data"
 
-var Files []File
 
-func listFiles(w http.ResponseWriter, r *http.Request){
-	json.NewEncoder(w).Encode(Files)
-}
-
-func homePage(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "Welcome to Go Go Drive!\n")
-}
-
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/files", listFiles)
-	log.Fatal(http.ListenAndServe(":8081", nil))
-}
-
-func populate(){
-	Files = []File{
-		File{Name: "log.txt"},
-		File{Name: "index.html"},
+func handleError(err error, message string, w http.ResponseWriter, statusCode int) {
+	if err != nil {
+		http.Error(w , "Failed to retrieve the file", statusCode)
+		return
 	}
 }
 
+func homePage(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
+	fmt.Fprint(w, "Welcome to Go Go Drive!\n")
+}
+
+func listFiles(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
+	
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
+	file, handler, err := r.FormFile("file")
+	handleError(err, "Failed to retrieve the file", w, http.StatusBadRequest)
+	defer file.Close()
+
+	err = os.MkdirAll(UploadDir, os.ModePerm)
+	handleError(err, "Failed to create the upload directory", w, http.StatusInternalServerError)
+
+	filePath := filepath.Join(UploadDir, handler.Filename)
+
+	// Create a new file on the server to save the uploaded file
+	destinationFile, err := os.Create(filePath)
+	handleError(err, "Failed to create the file on the server", w, http.StatusInternalServerError)
+	defer destinationFile.Close()
+
+	// Copy the uploaded file to the destination file on the server
+	_, err = io.Copy(destinationFile, file)
+	handleError(err, "Failed to save the uploaded file", w, http.StatusInternalServerError)
+	
+	// Return a response
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("File uploaded successfully!\n"))
+}
+
+func handleRequests() {
+	router := httprouter.New()
+	router.GET("/", homePage)
+	router.GET("/files", listFiles)
+	router.POST("/upload", uploadFile)
+	log.Fatal(http.ListenAndServe(":8081", router))
+}
+
 func main(){
-	populate()
 	handleRequests()
 }
